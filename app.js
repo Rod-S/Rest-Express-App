748472acbea29feb0bd77fe94b54e1acd30f760c
Rod S <33836692+Rod-S@users.noreply.github.com>
2019-03-30 17:00:43 -0500

    added routes.js for /questions routes

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 
 var express = require('express');
 var app = express();
-var jsonParser = require('body-parser').json
+var routes = require('./routes');
+
+var jsonParser = require('body-parser').json;
 
 app.use(jsonParser());
 
-app.use(function(req,res, next){
-  next();
-});
+app.use("/questions", routes);
 
 var port = process.env.PORT || 3000;
 

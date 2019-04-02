ae99d6ae30cfcc6cbb80410f96863e253b5e8792
Rod S <33836692+Rod-S@users.noreply.github.com>
2019-04-02 17:40:23 -0500

    -added models.js file with Question and Answer Schemas

---
+++ models.js
@@ -0,0 +1,24 @@
+'use strcit';
+
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var AnswerSchema = new Schema({
+  text: String,
+  createdAt: {type: Date, default: Date.now},
+  updatedAt: {type: Date, default: Date.now},
+  votes: {type: Number, default: 0}
+});
+
+var QuestionSchema = new Schema ({
+  text: String,
+  createdAt: {type: Date, default: Date.now},
+  answers: [AnswerSchema]
+});
+
+
+
+var Question = mongoose.model("Question", QuestionSchema);
+
+module.exports.Question = Question;

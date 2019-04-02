9097b7a0c16573ef9219a6c577ecb67e394d2854
Rod S <33836692+Rod-S@users.noreply.github.com>
2019-04-02 18:15:49 -0500

    -instance methods added to models.js

--- models.js
+++ models.js
@@ -4,11 +4,35 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
+var sortAnswers = function(a, b){
+  //- negative if a before before b
+  //0 no change
+  //+ positive if a after b
+  if(a.votes === b.votes){
+    return b.updatedAt - a.updatedAt;
+  }
+  return b.votes - a.votes;
+};
+
 var AnswerSchema = new Schema({
   text: String,
   createdAt: {type: Date, default: Date.now},
   updatedAt: {type: Date, default: Date.now},
   votes: {type: Number, default: 0}
+});
+
+AnswerSchema.method("update", function(updates, callback){
+  Object.assign(this, updates, {updatedAt: new Date()});
+  this.parent().save(callback);
+});
+
+AnswerSchema.method("vote", function(vote, callback){
+  if(vote === "up") {
+    this.votes += 1;
+  } else {
+    this.votes -= 1;
+  }
+  this.parent().save(callback);
 });
 
 var QuestionSchema = new Schema ({
@@ -17,7 +41,10 @@ var QuestionSchema = new Schema ({
   answers: [AnswerSchema]
 });
 
-
+QuestionSchema.pre("save", function(next){
+  this.answers.sort(sortAnswers);
+  next();
+});
 
 var Question = mongoose.model("Question", QuestionSchema);
 
